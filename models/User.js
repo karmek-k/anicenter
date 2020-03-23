3a3b259a9fd9db0481abb1bc494e7325f5697aa8
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-03-23 12:16:26 +0100

    Make the username field unique

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const db = require('../db');
 const User = db.define('User', {
   username: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   password: {
     type: DataTypes.STRING,
