805d79823fef0db51ca78f1542730256acc14f9c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-03-23 10:25:58 +0100

    Add SEQUELIZE_LOGGING

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const logging = Boolean(Number(process.env.SEQUELIZE_LOGGING));
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging
+});
 sequelize.authenticate()
   .then(() => console.log('Successfully connected to the database'))
   .catch(err => console.error(err));
