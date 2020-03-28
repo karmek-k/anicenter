beea8e2fda2621d40038c5ea55e307e8b9adb0a6
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-03-28 18:05:36 +0100

    Add some middleware

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const passport = require('passport');
+
+const morgan = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
@@ -8,11 +13,15 @@ const app = express();
 require('./db');
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use('/static',
   express.static(process.env.STATIC_PATH || 'static')
 );
 app.use(passport.initialize());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('dev'));
 
 // Passport config
 require('./config/passport')(passport);
