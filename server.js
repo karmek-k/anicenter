a51b9e7298e572f43690a09d8308f05dc2926d9a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-03-29 11:53:21 +0200

    Add express-fileupload

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const compression = require('compression');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
 require('dotenv').config();
 
 const app = express();
@@ -15,18 +17,22 @@ require('./db');
 // Middleware
 app.use(cookieParser());
 app.use(express.json());
-app.use('/static',
-  express.static(process.env.STATIC_PATH || 'static')
-);
-app.use(passport.initialize());
+app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static(process.env.STATIC_PATH || 'static'));
+app.use(passport.initialize({}));
 app.use(helmet());
 app.use(compression());
-app.use(morgan('dev'));
+app.use(fileUpload({}));
+
+if (process.env.NODE_ENV.toLowerCase() === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Passport config
 require('./config/passport')(passport);
 
 // Routes
 app.use('/users', require('./routes/users'));
+app.use('/files', require('./routes/files'));
 
 module.exports = app;
